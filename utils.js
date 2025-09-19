861a4dc49dcbc586e8434478c455faa14be9165b
Liam <56305595+LiamDochartaigh@users.noreply.github.com>
2025-09-19 14:01:44 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,6 +2,7 @@
   window.utils = {
     generateRandomHexId,
     hexToRgb,
+    getFilterFromHex,
     awaitAnimFrame,
     throttle,
     awaitDelay,
@@ -28,6 +29,7 @@
   var utils_default = {
     generateRandomHexId,
     hexToRgb,
+    getFilterFromHex,
     awaitAnimFrame,
     throttle,
     awaitDelay,
@@ -74,6 +76,226 @@
       { length },
       () => Math.floor(Math.random() * 16).toString(16)
     ).join("");
+  }
+  class Color {
+    r;
+    g;
+    b;
+    constructor(r, g, b) {
+      this.r = this.clamp(r);
+      this.g = this.clamp(g);
+      this.b = this.clamp(b);
+      this.set(r, g, b);
+    }
+    toString() {
+      return `rgb(${Math.round(this.r)}, ${Math.round(this.g)}, ${Math.round(this.b)})`;
+    }
+    set(r, g, b) {
+      this.r = this.clamp(r);
+      this.g = this.clamp(g);
+      this.b = this.clamp(b);
+    }
+    clamp(value) {
+      return Math.max(0, Math.min(255, value));
+    }
+    multiply(matrix) {
+      const newR = this.clamp(this.r * matrix[0] + this.g * matrix[1] + this.b * matrix[2]);
+      const newG = this.clamp(this.r * matrix[3] + this.g * matrix[4] + this.b * matrix[5]);
+      const newB = this.clamp(this.r * matrix[6] + this.g * matrix[7] + this.b * matrix[8]);
+      this.r = newR;
+      this.g = newG;
+      this.b = newB;
+    }
+    invert(value = 1) {
+      this.r = this.clamp((value + this.r / 255 * (1 - 2 * value)) * 255);
+      this.g = this.clamp((value + this.g / 255 * (1 - 2 * value)) * 255);
+      this.b = this.clamp((value + this.b / 255 * (1 - 2 * value)) * 255);
+    }
+    sepia(value = 1) {
+      this.multiply([
+        0.393 + 0.607 * (1 - value),
+        0.769 - 0.769 * (1 - value),
+        0.189 - 0.189 * (1 - value),
+        0.349 - 0.349 * (1 - value),
+        0.686 + 0.314 * (1 - value),
+        0.168 - 0.168 * (1 - value),
+        0.272 - 0.272 * (1 - value),
+        0.534 - 0.534 * (1 - value),
+        0.131 + 0.869 * (1 - value)
+      ]);
+    }
+    saturate(value = 1) {
+      this.multiply([
+        0.213 + 0.787 * value,
+        0.715 - 0.715 * value,
+        0.072 - 0.072 * value,
+        0.213 - 0.213 * value,
+        0.715 + 0.285 * value,
+        0.072 - 0.072 * value,
+        0.213 - 0.213 * value,
+        0.715 - 0.715 * value,
+        0.072 + 0.928 * value
+      ]);
+    }
+    hueRotate(angle = 0) {
+      angle = angle / 180 * Math.PI;
+      const sin = Math.sin(angle);
+      const cos = Math.cos(angle);
+      this.multiply([
+        0.213 + cos * 0.787 - sin * 0.213,
+        0.715 - cos * 0.715 - sin * 0.715,
+        0.072 - cos * 0.072 + sin * 0.928,
+        0.213 - cos * 0.213 + sin * 0.143,
+        0.715 + cos * 0.285 + sin * 0.14,
+        0.072 - cos * 0.072 - sin * 0.283,
+        0.213 - cos * 0.213 - sin * 0.787,
+        0.715 - cos * 0.715 + sin * 0.715,
+        0.072 + cos * 0.928 + sin * 0.072
+      ]);
+    }
+    brightness(value = 1) {
+      this.linear(value);
+    }
+    contrast(value = 1) {
+      this.linear(value, -(0.5 * value) + 0.5);
+    }
+    linear(slope = 1, intercept = 0) {
+      this.r = this.clamp(this.r * slope + intercept * 255);
+      this.g = this.clamp(this.g * slope + intercept * 255);
+      this.b = this.clamp(this.b * slope + intercept * 255);
+    }
+    hsl() {
+      const r = this.r / 255;
+      const g = this.g / 255;
+      const b = this.b / 255;
+      const max = Math.max(r, g, b);
+      const min = Math.min(r, g, b);
+      let h = 0, s = 0;
+      const l = (max + min) / 2;
+      if (max !== min) {
+        const d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+          case r:
+            h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+          case g:
+            h = (b - r) / d + 2;
+            break;
+          case b:
+            h = (r - g) / d + 4;
+            break;
+        }
+        h /= 6;
+      }
+      return { h: h * 100, s: s * 100, l: l * 100 };
+    }
+  }
+  class Solver {
+    target;
+    targetHSL;
+    reusedColor;
+    constructor(target) {
+      this.target = target;
+      this.targetHSL = target.hsl();
+      this.reusedColor = new Color(0, 0, 0);
+    }
+    solve() {
+      const result = this.solveNarrow(this.solveWide());
+      return {
+        values: result.values,
+        loss: result.loss,
+        filter: this.css(result.values)
+      };
+    }
+    solveWide() {
+      const A = 5;
+      const c = 15;
+      const a = [60, 180, 18e3, 600, 1.2, 1.2];
+      let best = { loss: Infinity, values: [] };
+      for (let i = 0; best.loss > 25 && i < 3; i++) {
+        const initial = [50, 20, 3750, 50, 100, 100];
+        const result = this.spsa(A, a, c, initial, 1e3);
+        if (result.loss < best.loss)
+          best = result;
+      }
+      return best;
+    }
+    solveNarrow(wide) {
+      const A = wide.loss;
+      const c = 2;
+      const A1 = A + 1;
+      const a = [0.25 * A1, 0.25 * A1, A1, 0.25 * A1, 0.2 * A1, 0.2 * A1];
+      return this.spsa(A, a, c, wide.values, 500);
+    }
+    spsa(A, a, c, values, iters) {
+      const alpha = 1;
+      const gamma = 1 / 6;
+      let best = [];
+      let bestLoss = Infinity;
+      for (let k = 0; k < iters; k++) {
+        const ck = c / Math.pow(k + 1, gamma);
+        const deltas = Array.from({ length: 6 }, () => Math.random() > 0.5 ? 1 : -1);
+        const highArgs = values.map((v, i) => v + ck * deltas[i]);
+        const lowArgs = values.map((v, i) => v - ck * deltas[i]);
+        const lossDiff = this.loss(highArgs) - this.loss(lowArgs);
+        for (let i = 0; i < 6; i++) {
+          const g = lossDiff / (2 * ck) * deltas[i];
+          const ak = a[i] / Math.pow(A + k + 1, alpha);
+          values[i] = this.fix(values[i] - ak * g, i);
+        }
+        const loss = this.loss(values);
+        if (loss < bestLoss) {
+          best = [...values];
+          bestLoss = loss;
+        }
+      }
+      return { values: best, loss: bestLoss };
+    }
+    fix(value, idx) {
+      let max = 100;
+      if (idx === 2)
+        max = 7500;
+      else if (idx === 4 || idx === 5)
+        max = 200;
+      if (idx === 3) {
+        if (value > max)
+          value %= max;
+        else if (value < 0)
+          value = max + value % max;
+      } else {
+        if (value < 0)
+          value = 0;
+        if (value > max)
+          value = max;
+      }
+      return value;
+    }
+    loss(filters) {
+      const color = this.reusedColor;
+      color.set(0, 0, 0);
+      color.invert(filters[0] / 100);
+      color.sepia(filters[1] / 100);
+      color.saturate(filters[2] / 100);
+      color.hueRotate(filters[3] * 3.6);
+      color.brightness(filters[4] / 100);
+      color.contrast(filters[5] / 100);
+      const colorHSL = color.hsl();
+      return Math.abs(color.r - this.target.r) + Math.abs(color.g - this.target.g) + Math.abs(color.b - this.target.b) + Math.abs(colorHSL.h - this.targetHSL.h) + Math.abs(colorHSL.s - this.targetHSL.s) + Math.abs(colorHSL.l - this.targetHSL.l);
+    }
+    css(filters) {
+      const fmt = (idx, multiplier = 1) => Math.round(filters[idx] * multiplier);
+      return `invert(${fmt(0)}%) sepia(${fmt(1)}%) saturate(${fmt(2)}%) hue-rotate(${fmt(3, 3.6)}deg) brightness(${fmt(4)}%) contrast(${fmt(5)}%)`;
+    }
+  }
+  function getFilterFromHex(hex) {
+    const rgb = hexToRgb(hex);
+    if (!rgb)
+      return null;
+    const color = new Color(rgb.r, rgb.g, rgb.b);
+    const solver = new Solver(color);
+    const result = solver.solve();
+    return result.filter;
   }
   function hexToRgb(hex, cssFormat = false) {
     hex = hex.replace(/^#/, "");
@@ -293,6 +515,20 @@
       img.src = url;
     });
   }
+  function loadVideo(url) {
+    return new Promise((resolve, reject) => {
+      const video = document.createElement("video");
+      video.onloadeddata = () => {
+        resolve(video);
+      };
+      video.onerror = () => {
+        console.error(`Failed to load video: ${url}`);
+        reject(new Error(`Failed to load video: ${url}`));
+      };
+      video.src = url;
+      video.load();
+    });
+  }
   function loadAudio(url) {
     return new Promise((resolve, reject) => {
       const audio = new Audio(url);
@@ -307,13 +543,14 @@
       audio.load();
     });
   }
-  async function createAssetManager(imageAssets, audioAssets, opts = {}) {
+  async function createAssetManager(imageAssets, audioAssets, opts = {}, videoAssets) {
     const defaultOpts = {
       ...defaultAssetManageOpts,
       ...opts
     };
     const imgAssetManager = /* @__PURE__ */ new Map();
     const audioAssetManager = /* @__PURE__ */ new Map();
+    const vidAssetManager = /* @__PURE__ */ new Map();
     const loadPromises = [];
     Object.entries(imageAssets).forEach(([key, url]) => {
       const loadPromise = new Promise(async (resolve, reject) => {
@@ -323,6 +560,16 @@
       });
       loadPromises.push(loadPromise);
     });
+    if (videoAssets) {
+      Object.entries(videoAssets).forEach(([key, url]) => {
+        const loadPromise = new Promise(async (resolve, reject) => {
+          const vid = await loadVideo(url);
+          vidAssetManager.set(key, vid);
+          resolve();
+        });
+        loadPromises.push(loadPromise);
+      });
+    }
     if (audioAssets && defaultOpts.preLoadAudio) {
       Object.entries(audioAssets).forEach(([key, url]) => {
         const loadPromise = new Promise(async (resolve, reject) => {
@@ -338,7 +585,9 @@
       getImageAsset: (key) => imgAssetManager.get(key),
       imageAssets: Array.from(imgAssetManager.entries()),
       getAudioPool: (key) => audioAssetManager.get(key),
-      audioAssets: Array.from(audioAssetManager.entries())
+      audioAssets: Array.from(audioAssetManager.entries()),
+      videoAssets: Array.from(vidAssetManager.entries()),
+      getVideoAssets: (key) => vidAssetManager.get(key)
     };
   }
   function createAudioPool(url, poolSize) {
